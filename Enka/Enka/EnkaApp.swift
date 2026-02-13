10202ba66c0559dc5c56f5bc375531033022d8ee
appus <sea-anemone12@icloud.com>
2026-02-13 14:01:13 +0900

    first commit

---
+++ Enka/Enka/EnkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnkaApp.swift
+//  Enka
+//
+//  Created by のりやまのりを on 2026/02/13.
+//
+
+import SwiftUI
+
+@main
+struct EnkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
